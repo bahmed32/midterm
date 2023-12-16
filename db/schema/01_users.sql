02f27630f429b044522dbbb80ed5c1a69921ff0d
Dylan Palin <dylanpalin@gmail.com>
2023-12-16 16:00:48 -0700

    added db schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,3 +5,36 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
 );
+
+-- Create Users table
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+-- Create Categories table
+CREATE TABLE Categories (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    keywords VARCHAR(100) NOT NULL
+);
+
+-- Create Lists table with foreign keys to Users and Categories
+CREATE TABLE Lists (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+    category_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (CategoryID) REFERENCES Categories(CategoryID)
+);
+
+-- Create Items table with foreign keys to Lists
+CREATE TABLE Items (
+    ItemID SERIAL PRIMARY KEY NOT NULL,
+    ListID INT,
+    ItemName VARCHAR(100) NOT NULL,
+    ItemCategory VARCHAR(20) NOT NULL, -- 'Book', 'Movie', 'Restaurant', 'Product'
+    FOREIGN KEY (ListID) REFERENCES Lists(ListID)
+);
