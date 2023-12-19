3b908340994cbb5e3f4eaa770e4a1bb92d47d560
Dylan Palin <dylanpalin@gmail.com>
2023-12-18 17:03:15 -0700

    added API key to helper function

--- routes/google_api_helper.js
+++ routes/google_api_helper.js
@@ -6,50 +6,46 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 
-  function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/cloud-language https://www.googleapis.com/auth/cloud-platform"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
-  function loadClient() {
-    gapi.client.setApiKey(process.env.GOOGLE_API_KEY);
-    return gapi.client.load("https://language.googleapis.com/$discovery/rest?version=v1")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
-    return gapi.client.language.documents.classifyText({
-      "resource": {
-        "document": {
-          "content": `Jim ${escape(user.avatars)}`,
-          "type": "PLAIN_TEXT",
-          "language": ""
-        },
-        "classificationModelOptions": {
-          "v2Model": {
-            "contentCategoriesVersion": "V2"
-          }
+
+function authenticate() {
+  return gapi.auth2.getAuthInstance()
+    .signIn({ scope: "https://www.googleapis.com/auth/cloud-language https://www.googleapis.com/auth/cloud-platform" })
+    .then(function () { console.log("Sign-in successful"); },
+      function (err) { console.error("Error signing in", err); });
+}
+function loadClient() {
+  gapi.client.setApiKey("e07522f3a06fde1a05eeef8cb61a122b4068da00");
+  return gapi.client.load("https://language.googleapis.com/$discovery/rest?version=v1")
+    .then(function () { console.log("GAPI client loaded for API"); },
+      function (err) { console.error("Error loading GAPI client for API", err); });
+}
+// Make sure the client is loaded and sign-in is complete before calling this method.
+function execute() {
+  return gapi.client.language.documents.classifyText({
+    "resource": {
+      "document": {
+        "content": `Jim Carrey`,
+        "type": "PLAIN_TEXT",
+        "language": ""
+      },
+      "classificationModelOptions": {
+        "v2Model": {
+          "contentCategoriesVersion": "V2"
         }
       }
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "269683579985-3af5iq7352uj1cus9o9j4mo1e28g5peu.apps.googleusercontent.com"});
-  });
+    }
+  })
+    .then(function (response) {
+      // Handle the results here (response.result has the parsed body).
+      console.log("Response", response);
+    },
+      function (err) { console.error("Execute error", err); });
+}
+gapi.load("client:auth2", function () {
+  gapi.auth2.init({ client_id: "269683579985-3af5iq7352uj1cus9o9j4mo1e28g5peu.apps.googleusercontent.com" });
+});
 
-  res.render('users');
-
-
-
-module.exports = router;
-module.exports = 
+module.exports = { router, authenticate, loadClient, execute };
