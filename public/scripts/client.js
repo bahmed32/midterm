66e967a584a38e88bad0a2aab5178943c15a3ef0
Dylan Palin <dylanpalin@gmail.com>
2023-12-19 22:21:13 -0700

    modified client.js file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,4 @@
-const { Pool } = require('pg');
-
+const gapi = require('gapi');
 // Client facing scripts here
 
 // Escape function to prevent XSS
@@ -12,104 +11,100 @@ const escape = function (str) {
 // Authenticate API key on document load
 
 $(document).ready(function () {
-  authenticate = () => {
-    console.log("authenticating");
+  function loadClient() {
+    console.log("#2 Loading Content");
+    gapi.client.setApiKey(process.env.API_KEY);
+    return gapi.client.load("https://language.googleapis.com/$discovery/rest?version=v1")
+      .then(function () { console.log("GAPI client loaded for API"); },
+        function (err) { console.error("Error loading GAPI client for API", err); });
+  }
+  function authenticate() {
+    console.log("#1 Authenticating");
     return gapi.auth2.getAuthInstance()
       .signIn({ scope: "https://www.googleapis.com/auth/cloud-language https://www.googleapis.com/auth/cloud-platform" })
-      .then(function () { console.log("Sign-in successful"); })
-      .catch(function (err) { console.error("Error signing in", err); });
+      .then(function () { console.log("Sign-in successful"); },
+        function (err) { console.error("Error signing in", err); });
   }
-  loadClient = () => {
-    console.log("loading client");
-    gapi.client.setApiKey(process.env.API_KEY);
-    gapi.client.load("https://language.googleapis.com/$discovery/rest?version=v1")
-      .then(function () {
-        console.log("GAPI client loaded for API");
-      })
-      .catch(function (err) {
-        console.error("Error loading GAPI client for API", err);
-      });
-  }
+  gapi.load("client:auth2", function () {
+    gapi.auth2.init({ client_id: "269683579985-3af5iq7352uj1cus9o9j4mo1e28g5peu.apps.googleusercontent.com" });  
+    authenticate().then(loadClient);
 });
 
 // Create list item element from list item data
-$("#taskInput").keyup(function (event) {
-  event.preventDefault();
-  const data = $(this).val();
-  gapi.client.language.documents.classifyText({
-    "resource": {
-      "document": {
-        "content": data,
-        "type": "PLAIN_TEXT",
-        "language": ""
-      },
-      "classificationModelOptions": {
-        "v2Model": {
-          "contentCategoriesVersion": "V2"
-        }
+
+
+    $("#addNew").on("submit", (event) => {
+      console.log("#3 Submitting");
+      event.preventDefault();
+      const data = $("#taskInput").val();
+      function execute() {
+        return gapi.client.language.documents.classifyText({
+          "resource": {
+            "document": {
+              "content": `${escape(data)}`,
+              "type": "PLAIN_TEXT",
+              "language": ""
+            },
+            "classificationModelOptions": {
+              "v2Model": {
+                "contentCategoriesVersion": "V2"
+              }
+            }
+          }
+        })
+          .then(function (response) {
+            return $.ajax({
+              type: "POST",
+              url: "/lists",
+              data: JSON.stringify(response), // Stringify the response object
+            });
+          },
+            function (err) { console.error("Execute error", err); });
       }
-    }
-  })
-    .then(function (response) {
-      console.log("Response", response);
-      return $.ajax({
-        type: "POST",
-        url: "/lists",
-        data: response,
-      });
-    })
-    .then(function (response) {
-      console.log("Response", response);
-    })
-    .catch(function (err) {
-      console.error("Execute error", err);
     });
+  });
 });
+//
 
-gapi.load("client:auth2", function () {
-  gapi.auth2.init({ client_id: "269683579985-3af5iq7352uj1cus9o9j4mo1e28g5peu.apps.googleusercontent.com" });
-});
+// //  validate and submit item when button is clicked
+//  $(document).ready(function () {
+//    const loadLists = function () {
+//      $.ajax('/lists', { method: 'POST' })
+//        .then(function (response) {
+//          console.log(response);
+//        })
+//    };
+//    loadLists()
+//  });
+//  $("form").submit(function (event) {
+//    event.preventDefault();
 
+//    let formData = {
+//      text: $('#taskInput').val()
+//    };
 
-// validate and submit item when button is clicked
-// $(document).ready(function () {
-//   const loadLists = function () {
-//     $.ajax('/lists', { method: 'POST' })
-//       .then(function (response) {
-//         console.log(response);
-//       })
-//   };
-//   loadLists()
-// });
-// $("form").submit(function (event) {
-//   event.preventDefault();
+//    if (formData.text.length > 140) {
+//      $('#textarea').addClass('error');
+//      $('#error').remove();
+//      $('#addButton').after('<p id="error">Your entry is too long!</p>');
+//    }
 
-//   let formData = {
-//     text: $('#textarea').val()
-//   };
+//    if (formData.text.length === 0) {
+//      $('#textarea').addClass('error');
+//      $('#error').remove();
+//      $('#addButton').after('<p id="error">Your entry is too short!</p>');
+//    }
 
-//   if (formData.text.length > 140) {
-//     $('#textarea').addClass('error');
-//     $('#error').remove();
-//     $('#addButton').after('<p id="error">Your entry is too long!</p>');
-//   }
+//    $.ajax({
+//      type: "POST",
+//      url: "/lists",
+//      data: formData,
+//    })
+//      .done(() => {
 
-//   if (formData.text.length === 0) {
-//     $('#textarea').addClass('error');
-//     $('#error').remove();
-//     $('#addButton').after('<p id="error">Your entry is too short!</p>');
-//   }
+//       //  $.get('/lists', function (theData) {
+//       //    $('.list-container-1').prepend(createListElement(theData[theData.length - 1]));
 
-//   $.ajax({
-//     type: "POST",
-//     url: "/lists",
-//     data: formData,
-//   })
-//     .done(() => {
-
-//       $.get('/lists', function (theData) {
-//         $('.list-container').prepend(createListElement(theData[theData.length - 1]));
-
-//       });
-//     });
-// });
+//       //  });
+//      });
+//  });
--- routes/lists.js
+++ routes/lists.js
@@ -6,27 +6,32 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const listQueries = require('../db/queries/lists');
 const coffescript = require('coffee-script/register');
 const fetchListsFromDatabase = require('../lib/data-helpers').fetchListsFromDatabase;
+const execute = require('../lib/data-helpers').excute;
 
 router.get('/lists', (req, res) => {
-  // Fetch the lists from your database
-  // const lists = fetchListsFromDatabase();
 
-  // // Send the lists as a JSON response
-  // res.json(lists);
+  // Fetch the lists from your database
+  const lists = fetchListsFromDatabase();
+
+  // Send the lists as a JSON response
+  res.json(lists);
 });
 
 router.post('/lists', (req, res) => {
-console.log('ASDFASDFASDFASDFASF', req.body);
-  const list = {
-    text: req.body.text
-  };
+  const data = req.body;
+  execute(data)
+    .then(function (response) {
+      res.json(response);
+    })
+    .catch(function (err) {
+      console.error("execute error", err);
+      res.status(500).json({ error: err.message });
+    });
 
-  // Send the list as a JSON response
-  res.json(list);
 });
 
 
