ca8fc84d20ef19d5717367f135770b5da0ce2e92
Dylan Palin <dylanpalin@gmail.com>
2023-12-20 22:45:42 -0700

    updated client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,49 +3,19 @@ const fs = require('fs');
 const language = require('@google-cloud/language');
 
 
-
-function classifyText() {
-  const text = $("text-box").val();
-  $.ajax({
-    url: '/list',
-    type: 'POST',
-    data: {
-      document: {
-        content: text,
-        type: 'PLAIN_TEXT',
-      },
-      classificationModelOptions: {
-        v2Model: {
-          contentCategoriesVersion: 'V2',
-        },
-      },
-    },
-    success: function(response) {
-      const categories = response.categories;
-      categories.forEach(category => {
-        console.log(`Name: ${category.name}`);
-        console.log(`Confidence: ${category.confidence}`);
-      });
-    },
-    error: function(error) {
-      console.log(error);
-    }
-  });
-};
-
-window.addEventListener('message', (event) => {
-  const message = event.data;
-
-  const newElement = document.createElement('div');
-  newElement.innerHTML = message.response;
-
-  document.body.appendChild(newElement);
+$("toDoform").submit(function (event) {
+  event.preventDefault();
+  function classifyText() {
+    const text = $("taskinput").val();
+    $.ajax({
+      url: '/categorize',
+      type: 'GET',
+      data: text,
+    })
+  }
+  console.log(data);
 });
 
-
-$(document).ready(function() {
-  $("classify-button").on("click", classifyText);
-})
 
 module.exports = {
   classifyText: classifyText,
