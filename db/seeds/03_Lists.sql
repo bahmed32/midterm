6280b9ae4e15ec8993c202deb2b38c2ab61db140
Dylan Palin <dylanpalin@gmail.com>
2023-12-21 15:37:57 -0700

    edited seeds and html

--- db/seeds/03_Lists.sql
+++ db/seeds/03_Lists.sql
@@ -1,6 +1,6 @@
 -- Lists table seeds here
 INSERT into lists (user_id, category_id) VALUES (1, 1);
-INSERT into lists (user_id, category_id) VALUES (2, 2);
-INSERT into lists (user_id, category_id) VALUES (3, 3);
-INSERT into lists (user_id, category_id) VALUES (4, 4);
+INSERT into lists (user_id, category_id) VALUES (1, 2);
+INSERT into lists (user_id, category_id) VALUES (1, 3);
+INSERT into lists (user_id, category_id) VALUES (1, 4);
 
