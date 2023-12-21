558e3003f2117138373fdcc2316b3525d0cd2ab0
Dylan Palin <dylanpalin@gmail.com>
2023-12-20 21:21:37 -0700

    removed broken functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,50 +1,7 @@
 // Client facing scripts here
-const fs = require('fs');
-const language = require('@google-cloud/language');
-
-
-
-function classifyText() {
-  const text = $("text-box").val();
-  $.ajax({
-    url: '/list',
-    type: 'POST',
-    data: {
-      document: {
-        content: text,
-        type: 'PLAIN_TEXT',
-      },
-      classificationModelOptions: {
-        v2Model: {
-          contentCategoriesVersion: 'V2',
-        },
-      },
-    },
-    success: function(response) {
-      const categories = response.categories;
-      categories.forEach(category => {
-        console.log(`Name: ${category.name}`);
-        console.log(`Confidence: ${category.confidence}`);
-      });
-    },
-    error: function(error) {
-      console.log(error);
-    }
-  });
-};
-
-window.addEventListener('message', (event) => {
-  const message = event.data;
-
-  const newElement = document.createElement('div');
-  newElement.innerHTML = message.response;
-
-  document.body.appendChild(newElement);
-});
-
 
 $(document).ready(function() {
-  $("classify-button").on("click", classifyText);
+  $("classify-button").on("click", classifyText($("input").val()));
 })
 
 module.exports = {
--- routes/lists.js
+++ routes/lists.js
@@ -9,13 +9,6 @@ const express = require('express');
 const router = express.Router();
 const listQueries = require('../db/queries/lists');
 const fetchListsFromDatabase = require('../lib/data-helpers').fetchListsFromDatabase;
-const execute = require('../lib/data-helpers').excute;
-
-const fs = require('fs');
-const key = fs.readFileSync('/home/labber/midterm/client_secret.json');
-const coffeescript = require('coffee-script/register');
-
-const client = new language.LanguageServiceClient({ keyFilename: '/home/labber/midterm/client_secret.json', });
 
 // GET /lists
 
